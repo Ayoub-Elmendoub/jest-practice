32d95713d1ca57fa61df0c334594dba163a592ad
Ayoub-Elmendoub <a.elmendoub@gmail.com>
2023-04-23 18:48:44 +0000

    refactor: remove ts use

--- user.spec.js
+++ user.spec.js
@@ -1,4 +1,22 @@
-import User from "./user";
+interface UserDetails {
+  firstName: string;
+  lastName: string;
+}
+
+class User {
+  private firstName;
+  private lastName;
+  constructor(details: UserDetails) {
+    const { firstName, lastName } = details;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get name() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
 describe("User: ", () => {
   it("name returns full name", () => {
     const user = new User({ firstName: "Jane", lastName: "Doe" });
